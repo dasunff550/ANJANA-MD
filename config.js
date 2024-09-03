cc5bb800605795ab56cabf061221264f4da6ab24
Dasun_Max <dasunffbrazil970@gmail.com>
2024-09-03 19:47:25 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,4 +9,5 @@ SESSION_ID: process.env.SESSION_ID,
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/1636c1eb652c31db7c8a2.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "👋𝗛𝗶⚡ 𝗕𝗥𝗢,I AM ALIVE NOW",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+MODE: process.env.MODE || "public",
 };
